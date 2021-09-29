0bfac40da6fd63c56d09d7dead8da6c0675abb71
Nick Grealy <nickgrealy@gmail.com>
2021-09-29 13:59:00 +1000

    Changed preview image.

--- ui/site-config.js
+++ ui/site-config.js
@@ -4,8 +4,8 @@ module.exports = {
   title: 'Chikn NFT',
   description: '8,008 algorithmically generated, unique Chikn.',
   url: 'https://chickenrun-rho.vercel.app/',
-  imageUrl: 'https://chickenrun-rho.vercel.app/brand/chicken-icon-white.png',
-  imageDimensionsPixels: '132',
+  imageUrl: 'https://chickenrun-rho.vercel.app/brand/chicken-preview-800px.jpg',
+  imageDimsPx: '800',
   themeColour: '#ffffff',
   keywords: 'avax,nft,chikn,chicken',
   author: 'noreply@chikn.io'
