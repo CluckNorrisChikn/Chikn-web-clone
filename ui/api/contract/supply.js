b47568ddadc99ba80dc6aa2f9399b8748e858809
Nick Grealy <nickgrealy@gmail.com>
2021-10-03 03:04:55 +1100

    Fixed imports.

--- ui/api/contract/supply.js
+++ ui/api/contract/supply.js
@@ -1,11 +1,11 @@
-const Contract = require('../../contract/Contract.class')
+const Contract = require('../../contract/ChickenContract.class')
 
 /** Returns the current minted. */
 const service = async (req, res) => {
   const contract = new Contract()
-  const [ minted, total ] = await Promise.all([
+  const [minted, total] = await Promise.all([
     contract.mintedCount(),
-    contract.totalCount(),
+    contract.totalCount()
   ])
   res.setHeader('Cache-Control', 's-max-age=5')
   res.json({ minted, total })
--- ui/contract/ChickenContract.class.js
+++ ui/contract/ChickenContract.class.js
@@ -1,19 +1,19 @@
 // const Web3 = require('web3')
-const { abi, address } = require('../../contract/Chicken_Fuji.json')
+const { abi, address } = require('./Chicken_Fuji.json')
 const Web3Eth = require('web3-eth')
 
 class Contract {
-  constructor(){
+  constructor() {
     const web3 = new Web3Eth(process.env.WEB3_URL || 'https://api.avax-test.network/ext/bc/C/rpc')
     this.daiToken = new web3.Contract(abi, address)
   }
 
   async mintedCount() {
-    return daiToken.methods.totalSupply().call()
+    return this.daiToken.methods.totalSupply().call()
   }
 
   async totalCount() {
-    return daiToken.methods.maxSupply().call()
+    return this.daiToken.methods.maxSupply().call()
   }
 }
 
