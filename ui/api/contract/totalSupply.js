976ef08dedb87837c5dee0912ac96ac1108131bf
Nick Grealy <nickgrealy@gmail.com>
2021-10-03 01:40:58 +1000

    minimized deps.

--- ui/api/contract/totalSupply.js
+++ ui/api/contract/totalSupply.js
@@ -1,5 +1,6 @@
-const Web3 = require('web3')
+// const Web3 = require('web3')
 const { abi, address } = require('../../contract/Chicken_Fuji.json')
+const Web3Eth = require('web3-eth')
 
 const url = process.env.WEB3_URL || 'https://api.avax-test.network/ext/bc/C/rpc'
 
@@ -8,10 +9,11 @@ let daiToken = null
 /** Returns the current minted. */
 const service = async (req, res) => {
   if (daiToken === null) {
-    const web3 = new Web3(new Web3.providers.HttpProvider(url))
-    daiToken = new web3.eth.Contract(abi, address)
+    const web3 = new Web3Eth(url)
+    daiToken = new web3.Contract(abi, address)
   }
   const minted = await daiToken.methods.totalSupply().call()
+  console.log('minted', minted)
   res.json({ minted })
 }
 
