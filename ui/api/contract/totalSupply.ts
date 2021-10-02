dfb3eacc0bfbdb7dce24dcb7a6c921d5a0869346
Nick Grealy <nickgrealy@gmail.com>
2021-10-03 01:08:14 +1000

    Added typescript conventions.

--- ui/api/contract/totalSupply.ts
+++ ui/api/contract/totalSupply.ts
@@ -1,15 +1,19 @@
-const Web3 = require('web3')
-const { abi, address } = require('../../contract/Chicken_Fuji.json')
+
+
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import Web3 from 'web3'
+import { abi, address } from './Chicken_Fuji.json'
 
 const url = process.env.WEB3_URL || 'https://api.avax-test.network/ext/bc/C/rpc'
 
 let daiToken = null
 
 /** Returns the current minted. */
-module.exports = async (req, res) => {
+
+export default async (req: VercelRequest, res: VercelResponse) => {
   if (daiToken === null) {
     const web3 = new Web3(new Web3.providers.HttpProvider(url))
-    daiToken = new web3.eth.Contract(abi, address)
+    daiToken = new web3.eth.Contract(abi as any, address)
   }
   const minted = await daiToken.methods.totalSupply().call()
   res.json({ minted })
