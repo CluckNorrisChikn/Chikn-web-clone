962503119c53652259d4d2df8a16ed6906ad2ef3
Nick Grealy <nickgrealy@gmail.com>
2021-10-03 01:33:06 +1000

    Play with node_modules.

--- ui/api/contract/totalSupply.js
+++ ui/api/contract/totalSupply.js
@@ -1,20 +1,18 @@
-
-
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import Web3 from 'web3'
-import { abi, address } from './Chicken_Fuji.json'
+const Web3 = require('web3')
+const { abi, address } = require('../../contract/Chicken_Fuji.json')
 
 const url = process.env.WEB3_URL || 'https://api.avax-test.network/ext/bc/C/rpc'
 
 let daiToken = null
 
 /** Returns the current minted. */
-
-export default async (req: VercelRequest, res: VercelResponse) => {
+const service = async (req, res) => {
   if (daiToken === null) {
     const web3 = new Web3(new Web3.providers.HttpProvider(url))
-    daiToken = new web3.eth.Contract(abi as any, address)
+    daiToken = new web3.eth.Contract(abi, address)
   }
   const minted = await daiToken.methods.totalSupply().call()
   res.json({ minted })
 }
+
+module.exports = service
