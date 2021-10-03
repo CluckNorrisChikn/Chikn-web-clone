b42004ffa4359277a01a8d17021c17304de69938
Sam Wei <samw2k00@gmail.com>
2021-10-03 13:06:09 +1100

    add injection to wallet

--- ui/apisrc/ChickenContract.class.js
+++ ui/apisrc/ChickenContract.class.js
@@ -5,21 +5,21 @@ const Web3Eth = require('web3-eth')
 class ChickenContract {
   constructor() {
     const web3 = new Web3Eth(process.env.WEB3_URL || 'https://api.avax-test.network/ext/bc/C/rpc')
-    this.daiToken = new web3.Contract(abi, address)
+    this.contract = new web3.Contract(abi, address)
   }
 
   /**
    * @returns {number} the count of minted tokens.
    */
   async mintedCount() {
-    return this.daiToken.methods.totalSupply().call()
+    return this.contract.methods.totalSupply().call()
   }
 
   /**
    * @returns {number} the count of total tokens.
    */
   async totalCount() {
-    return this.daiToken.methods.maxSupply().call()
+    return this.contract.methods.maxSupply().call()
   }
 }
 
