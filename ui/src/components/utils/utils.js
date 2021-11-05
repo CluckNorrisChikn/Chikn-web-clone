8c0c693c916a1885d85e21b2a8de9b03f125cf2b
Nick Grealy <nickgrealy@gmail.com>
2021-11-05 19:39:46 +1100

    Added filters to marketplace.

---
+++ ui/src/components/utils/utils.js
@@ -0,0 +1,14 @@
+
+/**
+ * Checks if two string arrays are NOT equal.
+ * @param {string[]} s1
+ * @param {string[]} s2
+ * @returns
+ */
+export const stringArraysNotEqual = (s1, s2) => {
+  if (!Array.isArray(s1) || !Array.isArray(s2)) {
+    return s1 !== s2
+  }
+  return s1.length !== s2.length ||
+    s1.sort().join(',') !== s2.sort().join(',')
+}
