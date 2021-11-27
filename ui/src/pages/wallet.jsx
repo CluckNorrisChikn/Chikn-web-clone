669a97ee70d98323b003c564fb1eede2810c12c2
Dean Schmid <dschmid00@gmail.com>
2021-11-27 11:12:33 +0700

    added pagination to wallet

--- ui/src/pages/wallet.jsx
+++ ui/src/pages/wallet.jsx
@@ -1,19 +1,19 @@
 /* eslint-disable no-unused-vars */
 import { navigate } from 'gatsby-link'
 import * as React from 'react'
-import { Alert, Button, Col, Row } from 'react-bootstrap'
+import { Alert, Button, Col, Row, Pagination } from 'react-bootstrap'
 import { FaSync } from 'react-icons/fa'
 import { useQueryClient } from 'react-query'
 import {
   ChickenCardShimmerx4,
-  ChickenCardWalletSummary
+  ChickenCardWalletSummary,
 } from '../components/ChickenCard'
 import { ChiknText, Section, StackRow } from '../components/Common'
 import {
   getErrorMessage,
   KEYS,
   useGetWalletTokensQuery,
-  useWeb3Contract
+  useWeb3Contract,
 } from '../components/Connect'
 import Layout from '../components/Layout'
 
@@ -23,6 +23,32 @@ const IndexPage = () => {
   const useWalletTokens = useGetWalletTokensQuery(contract, account, active)
 
   const { data: tokens = [] } = useWalletTokens
+
+  console.log('i am token', tokens)
+
+  const PAGE_SIZE = 5
+  const [pageNumber, setInternalPageNumber] = React.useState(0)
+  const maxPageNumber = React.useMemo(() => {
+    const total = tokens.length
+    const remainder = total % PAGE_SIZE
+    const maxPage = parseInt(total / PAGE_SIZE) + (remainder > 0 ? 0 : -1)
+    return maxPage
+  }, [tokens])
+
+  React.useEffect(() => {
+    if (pageNumber > maxPageNumber) setInternalPageNumber(0)
+  }, [pageNumber, maxPageNumber])
+
+  const setPage = React.useCallback(
+    (page, jumpToTop = false) => {
+      if (page < 0) setInternalPageNumber(0)
+      else if (page > maxPageNumber) setInternalPageNumber(maxPageNumber)
+      else setInternalPageNumber(page)
+      // only jump to top if coming from bottom...
+      if (jumpToTop) scrollToTopRef.current.scrollIntoView()
+    },
+    [maxPageNumber]
+  )
 
   return (
     <Layout pageName="Wallet">
@@ -68,30 +94,49 @@ const IndexPage = () => {
           !useWalletTokens.isFetching &&
           useWalletTokens.isSuccess &&
           tokens.length > 0 && (
-          <>
-            <h5>Total: {tokens.length.toLocaleString()}</h5>
-            <Row className="gy-3 gx-3 mt-4">
-              {tokens
-                .sort((a, b) => a - b)
-                .map((tokenId) => (
-                  <Col key={tokenId} sm={6} md={4} lg={3}>
-                    <ChickenCardWalletSummary
-                      tokenId={tokenId}
-                      onClick={() =>
-                        navigate(`/chikn/${tokenId}`, {
-                          state: {
-                            backLink: '/wallet',
-                            backLabel: 'Back to Wallet'
-                          }
-                        })
-                      }
-                    />
-                  </Col>
-                ))}
-            </Row>
-          </>
-        )}
+            <>
+              <h5>Total: {tokens.length.toLocaleString()}</h5>
+              <Row className="gy-3 gx-3 mt-4">
+                {tokens
+                  .slice(pageNumber * PAGE_SIZE, (pageNumber + 1) * PAGE_SIZE)
+                  .sort((a, b) => a - b)
+                  .map((tokenId) => (
+                    <Col key={tokenId} sm={6} md={4} lg={3}>
+                      <ChickenCardWalletSummary
+                        tokenId={tokenId}
+                        onClick={() =>
+                          navigate(`/chikn/${tokenId}`, {
+                            state: {
+                              backLink: '/wallet',
+                              backLabel: 'Back to Wallet',
+                            },
+                          })
+                        }
+                      />
+                    </Col>
+                  ))}
+              </Row>
+            </>
+          )}
       </Section>
+      <Pagination>
+        <Pagination.First
+          disabled={pageNumber === 0}
+          onClick={() => setPage(0)}
+        />
+        <Pagination.Prev
+          disabled={pageNumber === 0}
+          onClick={() => setPage(pageNumber - 1)}
+        />
+        <Pagination.Next
+          disabled={pageNumber === maxPageNumber}
+          onClick={() => setPage(pageNumber + 1)}
+        />
+        <Pagination.Last
+          disabled={pageNumber === maxPageNumber}
+          onClick={() => setPage(999999)}
+        />
+      </Pagination>
     </Layout>
   )
 }
